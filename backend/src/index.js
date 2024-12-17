4296b5d99d985d15879c3d299a16d6094d47dc63
Sweety Nandi <sweetynandi06062002@gmail.com>
2024-12-17 13:40:51 +0530

    add .env example file

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/weather', weatherRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 (async () => {
     await sql.sync()
